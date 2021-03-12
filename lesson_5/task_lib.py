27eb2495307d27127f79a0914ac2c347fac9aec8
Dmitry S. Danilov <dmitry.s.danilov@gmail.com>
2021-03-12 18:12:36 +0300

    Lesson 5: update a task
    Update solutions of the task problems.
    
    On branch lesson_5
    Changes to be committed:
    	modified:   lesson_5/problem_1.ipynb
    	modified:   lesson_5/problem_2.ipynb
    	modified:   lesson_5/problem_3.ipynb
    	modified:   lesson_5/problem_4.ipynb
    	modified:   lesson_5/task_lib.py

--- lesson_5/task_lib.py
+++ lesson_5/task_lib.py
@@ -1,18 +1,33 @@
 from scipy.stats import norm, t
+from functools import partial
 
 
-def H1(z, A, n=None):
+def significance_interval(A, n=None):
+    ppf = partial(t.ppf, df = n - 1) if n is not None else norm.ppf
+    a = A / 2, 1 - A / 2    
+    return ppf(a)
+
+
+def confidence_interval(E, S, n, z_a):
+    return [E - _ * S / n**.5 for _ in z_a[::-1]]
+
+
+def alternative_hypothesis_1(z, A, n=None):
+    ppf = partial(t.ppf, df = n - 1) if n is not None else norm.ppf
     a = A / 2, 1 - A / 2
-    z_a = t.ppf(a, n - 1) if n is not None else norm.ppf(a)
+    z_a = ppf(a)
     return z < z_a[0] or z_a[1] < z
 
 
-def H1_less(z, A, n=None):
-    z_a = t.ppf(A, n - 1) if n is not None else norm.ppf(A)
+def alternative_hypothesis_1_less(z, A, n=None):
+    ppf = partial(t.ppf, df = n - 1) if n is not None else norm.ppf
+    a = A
+    z_a = ppf(a)
     return z < z_a
 
 
-def H1_more(z, A, n=None):
+def alternative_hypothesis_1_more(z, A, n=None):
+    ppf = partial(t.ppf, df = n - 1) if n is not None else norm.ppf
     a = 1 - A
-    z_a = t.ppf(a, n - 1) if n is not None else norm.ppf(a)
+    z_a = ppf(a)
     return z_a < z
