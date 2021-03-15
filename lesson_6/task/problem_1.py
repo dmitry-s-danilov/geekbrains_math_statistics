cb2d297f72faadb28f734f3ec41cf0655641ff46
Dmitry S. Danilov <dmitry.s.danilov@gmail.com>
2021-03-15 18:00:25 +0300

    Lesson 6: make a task
    Add solution of one more task problem.
    
    On branch lesson_6
    Changes to be committed:
    	modified:   .gitignore
    	new file:   history.md
    	new file:   lesson_6/task/problem_1.ipynb
    	new file:   lesson_6/task/problem_1.py
    	new file:   lesson_6/task/problem_1_lib.py
    	modified:   lesson_6/task/problem_2.ipynb
    	new file:   lesson_6/task/problem_2.py
    	renamed:    lesson_6/task/task_lib.py -> lesson_6/task/problem_2_lib.py
    	modified:   lesson_6/task/problem_3.ipynb
    	new file:   requirements.txt

---
+++ lesson_6/task/problem_1.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from problem_1_lib import covariance, correlation_coefficient
+
+# Set data.
+x = [35, 45, 190, 200, 40, 70, 54, 150, 120, 110]
+y = [401, 574, 874, 919, 459, 739, 653, 902, 746, 832]
+
+# Find covariances and correlations.
+results = {
+    'x-variance': [
+        covariance(x, x),
+        np.cov(np.array(x), bias=True),
+        pd.DataFrame({'x': x, 'y': y}).cov(ddof=0).loc['x', 'x']
+    ],
+
+    'y-variance': [
+        covariance(y, y),
+        np.cov(np.array(y), bias=True),
+        pd.DataFrame({'x': x, 'y': y}).cov(ddof=0).loc['y', 'y']
+    ],
+            
+    'xy-covariance': [
+        covariance(x, y),
+        np.cov(np.array(x), np.array(y), bias=True)[0, 1],
+        pd.DataFrame({'x': x, 'y': y}).cov(ddof=0).loc['x', 'y']
+    ],
+    
+    'xy-correlation': [
+        correlation_coefficient(x, y),
+        np.corrcoef(np.array(x), np.array(y))[0, 1],
+        pd.DataFrame({'x': x, 'y': y}).corr().loc['x', 'y']        
+    ]
+}
+
+# Сheck the equality of results with the selected precision.
+precision = 6
+checks = {
+    key: all([round(_ - value[0], precision) == 0 for _ in value[1:]])
+    for key, value in results.items()
+}
+
+# Print results and checks.
+for key in results:
+    print(f'{key}: {results[key][0]:.6} {checks[key]}')
